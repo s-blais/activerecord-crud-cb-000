d6f7bbcff76f1ad1d17759322e0ff64929674165
Stephen Blais <stephen@s-blais.com>
2019-10-15 20:10:13 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,9 +1,3 @@
-# Replace the '__' in the below methods to to make the specs pass!
-# Each '__' corresponds to a single line of code you will need to write.
-# See the example below on the left and how it should look on the right.
-# def make_a_new_movie_instance    # def make_a_new_movie_instance
-#   movie = __                     #   movie = Movie.new
-# end                              # end
 
 def can_be_instantiated_and_then_saved
   movie = Movie.new
